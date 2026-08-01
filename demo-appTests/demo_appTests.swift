c2a104e7e261afc33ed694c80f5ad57d11223c4a
Ruobin <ruobin.wang.us@gmail.com>
2026-08-02 00:17:13 +0800

    Initial Commit

---
+++ demo-appTests/demo_appTests.swift
@@ -0,0 +1,19 @@
+//
+//  demo_appTests.swift
+//  demo-appTests
+//
+//  Created by robert on 8/2/26.
+//
+
+import Testing
+@testable import demo_app
+
+struct demo_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
